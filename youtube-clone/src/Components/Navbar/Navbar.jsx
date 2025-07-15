fddd9f9cd40cad7568a63968c783daaa58bc652e
Henrik <Henrikmattsson89@gmail.com>
2025-07-15 11:26:57 +0200

    feature: navbarfeels ready

--- youtube-clone/src/Components/Navbar/Navbar.jsx
+++ youtube-clone/src/Components/Navbar/Navbar.jsx
@@ -25,14 +25,16 @@ function Navbar() {
 			</div>
 
 			<div className="nav-middle flex-div">
-				<input
-					type="text"
-					placeholder="Search"
-				/>
-				<img
-					src={search_icon}
-					alt=""
-				/>
+				<div className="search-box flex-div">
+					<input
+						type="text"
+						placeholder="Search"
+					/>
+					<img
+						src={search_icon}
+						alt="Search icon"
+					/>
+				</div>
 			</div>
 
 			<div className="nav-right flex-div">
